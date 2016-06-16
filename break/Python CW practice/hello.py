bd8af1647a39193e5d90b4393ff76504ef0cea33
attilakrupl <krupl.attila@gmail.com>
2016-06-16 17:36:19 +0100

    python practice - CW - hello

---
+++ break/Python CW practice/hello.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def __init__(self):
+        self.message = "Hello World!"
+    def main(self, param):
+        return self.message
+
+Solution = Solution()
+print (Solution.main("mizu"))
