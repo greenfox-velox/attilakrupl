e0ca16bed9d09d567efab6abb83620b8ef1806d5
attilakrupl <krupl.attila@gmail.com>
2016-06-20 11:14:57 +0100

    python practice - CW - The Fusc Function

---
+++ break/Python CW Practice week 2/TheFuscFunction.py
@@ -0,0 +1,14 @@
+def fusc(n):
+    assert type(n) == int and n >= 0
+    if n == 0:
+        return 0
+    elif n == 1: 
+        return 1 
+    elif n % 2 == 0:
+        return fusc(int(n/2))
+    else:
+        return fusc(int((n-1)/2)) + fusc(int((n-1)/2)+1)
+    
+    
+    
+print (fusc(25))
