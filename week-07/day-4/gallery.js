b6aee44877aaae2e30703c290eb020b038769bdf
attilakrupl <krupl.attila@gmail.com>
2016-07-01 08:12:40 +0100

    gallery.js working

--- week-07/day-4/gallery.js
+++ week-07/day-4/gallery.js
@@ -1,23 +1,119 @@
 'use strict';
+// const fs = require('fs');
+// function getFiles(dir) {
+//   const fileList = [];
+//   const files = fs.readdirSync(dir);
+//   for (var i in files) {
+//     if (!files.hasOwnProperty(i)) continue;
+//     const name = dir+'/'+files[i];
+//     if (!fs.statSync(name).isDirectory()) {
+//       fileList.push(name);
+//     }
+//   }
+//   return fileList;
+// }
+// const fileList = getFiles('/images');
+// console.log(fileList);
+let imageCounter = 0;
+let thumbCounter = 0;
+const fileList = ['Ceres-Bright-Spot',
+                  'Saturn',
+                  'Saturn-F-Ring',
+                  'Epimetheus',
+                  'Enceladus',
+                  'Ceres-Ridges',
+                  'Enceladus-Tectonic',
+                  'Saturn-Rings',
+                  'Saturn-Rings-2',
+                  'Dione'];
+const numOfImages = fileList.length;
+const bigButtonLeft = document.querySelector('#button1');
+const bigButtonRight = document.querySelector('#button2');
+const smallButtonLeft = document.querySelector('#button3');
+const smallButtonRight = document.querySelector('#button4');
+// const thumb1 = document.querySelector('#thumb1');
+// const thumb2 = document.querySelector('#thumb2');
+// const thumb3 = document.querySelector('#thumb3');
+// const thumb4 = document.querySelector('#thumb4');
+// const thumb5 = document.querySelector('#thumb5');
 
-const fileList = ['./images/01.jpg',
-                  './images/02.jpg',
-                  './images/03.jpg',
-                  './images/04.jpg',
-                  './images/05.jpg',
-                  './images/06.jpg',
-                  './images/07.jpg',
-                  './images/08.jpg',
-                  './images/09.jpg',
-                  './images/10.jpg'];
+function fileNameGenerator(num) {
+  const fileName = fileList[((imageCounter) + num) % numOfImages];
+  return './images/' + fileName + '.jpg';
+}
 
-const startingSetting = new function() {
-  document.querySelector('.main-image').setAttribute('src', fileList[0]);
-};
+function thumbNameGenerator(num) {
+  const fileName = fileList[((thumbCounter) + num) % numOfImages];
+  return './images/' + fileName + '.jpg';
+}
 
-console.log(document.querySelectorAll('.thumbnail'));
-document.querySelectorAll('.thumbnail')[0].setAttribute('src', fileList[0]);
-document.querySelectorAll('.thumbnail')[1].setAttribute('src', fileList[1]);
-document.querySelectorAll('.thumbnail')[2].setAttribute('src', fileList[2]);
-document.querySelectorAll('.thumbnail')[3].setAttribute('src', fileList[3]);
-document.querySelectorAll('.thumbnail')[4].setAttribute('src', fileList[4]);
+function setName() {
+  document.querySelector('.image_name').textContent = fileList[imageCounter % numOfImages];
+}
+
+function setImages() {
+  document.querySelectorAll('.main-image')[0].setAttribute('src', fileNameGenerator(0));
+  for (let i = 0; i < 5; i++) {
+    document.querySelectorAll('.thumbnail')[i].setAttribute('src', fileNameGenerator(i));
+  }
+  document.querySelectorAll('.thumbnail')[0].classList.add('highlighted');
+}
+
+function moveImages() {
+  document.querySelectorAll('.main-image')[0].setAttribute('src', fileNameGenerator(0));
+  thumbCounter = imageCounter;
+}
+
+function moveThumbs() {
+  for (let i = 0; i < 5; i++) {
+    document.querySelectorAll('.thumbnail')[i].setAttribute('src', thumbNameGenerator(i));
+  }
+}
+
+
+function goLeft() {
+  if (imageCounter > 0) {
+    imageCounter--;
+  } else {
+    imageCounter += numOfImages - 1;
+  }
+  moveImages();
+  moveThumbs();
+  setName();
+}
+
+function goRight() {
+  imageCounter++;
+  moveImages();
+  moveThumbs();
+  setName();
+}
+
+function thumbLeft() {
+  if (thumbCounter > 0) {
+    thumbCounter--;
+  } else {
+    thumbCounter += numOfImages - 1;
+  }
+  moveThumbs();
+}
+
+function thumbRight() {
+  thumbCounter++;
+  moveThumbs();
+}
+
+// function highlightThumb(id) {
+//   for (let i in document.querySelectorAll('.thumbnail')) {
+//     i.classList.remove('highlighted');
+//   }
+//   document.querySelector(id).classList.add('highlighted');
+// }
+
+setImages();
+setName();
+bigButtonLeft.addEventListener('click', goLeft);
+bigButtonRight.addEventListener('click', goRight);
+smallButtonLeft.addEventListener('click', thumbLeft);
+smallButtonRight.addEventListener('click', thumbRight);
+// thumb1.addEventListener('click', goLeft);
