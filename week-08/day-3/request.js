9161b603bfd6276424d8026d4e334a12ecee5b76
attilakrupl <krupl.attila@gmail.com>
2016-07-07 09:49:35 +0100

    yodanizer raw html done

--- week-08/day-3/request.js
+++ week-08/day-3/request.js
@@ -4,8 +4,7 @@ const runTheRequest = document.querySelector('.run_the_request');
 const arr1 = [];
 
 function doRequest() {
-  arr1.push(document.querySelector('.request_script').src = 'req.js');
-  
+  (document.querySelector('.request_script').src = 'req.js');
 }
 
 
