3addd7bb0be923ea4f1ff785d43939096a85f334
attilakrupl <krupl.attila@gmail.com>
2016-07-07 09:09:25 +0100

    request from day3

--- week-08/day-3/req.js
+++ week-08/day-3/req.js
@@ -1,7 +1,10 @@
 
 const xhr = new XMLHttpRequest();
 xhr.onload = () => {
-  console.log(xhr.response);
+  let data = JSON.parse(xhr.response);
+  console.log(data);
+  alert(data.weekday);
+  alert(data.celebrations.length);
 };
 xhr.open('GET', 'http://calapi.inadiutorium.cz/api/v0/en/calendars/default/2015/6/27')
 xhr.send();
--- week-08/day-3/request.js
+++ week-08/day-3/request.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const runTheRequest = document.querySelector('.run_the_request');
+const arr1 = [];
 
 function doRequest() {
-  document.querySelector('.request_script').src = 'req.js';
+  arr1.push(document.querySelector('.request_script').src = 'req.js');
+  
 }
+
 
 runTheRequest.addEventListener('click', doRequest);
