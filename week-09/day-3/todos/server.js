d62d25b335de3ef9c1db643a769bd50ceb81752a
attilakrupl <krupl.attila@gmail.com>
2016-07-15 10:55:40 +0100

    subFunctions extracted

--- week-09/day-3/todos/server.js
+++ week-09/day-3/todos/server.js
@@ -1,59 +1,53 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
+const subFunctions = require('./subFunctions');
+
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'alma123',
   database: 'todos',
 });
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(express.static('todo'));
 
-function errorHandling(err) {
-  if (err) {
-    console.log(err.toString());
-    return;
-  }
-}
-
 app.get('/todos', (req, res) => {
   con.query('SELECT * from todos WHERE destroyed = 0;', (err, rows) => {
-  errorHandling(err);
-  res.send(JSON.parse(JSON.stringify(rows)));
+  subFunctions.errorHandling(err);
+  res.send(rows);
   });
 });
 
 app.get('/todos/:id', (req, res) => {
   con.query('SELECT * from todos WHERE id = ?', req.params.id, (err, rows) => {
-  errorHandling(err);
-  res.send(JSON.stringify(rows));
+  subFunctions.errorHandling(err);
+  res.send(rows);
   });
 });
 
 app.post('/todos/', (req, res) => {
-  var newLine = { completed : 0 , text:  req.body.text , destroyed : 0 };
-  con.query('INSERT INTO todos SET ?', newLine, (err, rows) => {
-  errorHandling(err);
-  res.send(JSON.stringify(rows));
+  con.query('INSERT INTO todos SET ?', subFunctions.setNewTodo(req.body.text), (err, rows) => {
+  subFunctions.errorHandling(err);
+  res.send(rows);
   });
 });
 
 app.put('/todos/:id', (req, res) => {
-  var newObj = (JSON.parse(JSON.stringify(req.body)));
-  var setTo = newObj.completed ? 1 : 0;
   con.query('UPDATE todos SET text = ?, completed = ? Where ID = ?',
-  [newObj.text, setTo, req.params.id], (err, rows) => {
-  errorHandling(err);
-  res.send(JSON.stringify(rows));
+  [subFunctions.defineBody(req.body).text, subFunctions.checkOrUncheck(req.body), req.params.id], (err, rows) => {
+  subFunctions.errorHandling(err);
+  res.send(rows);
   });
 });
 
 app.delete('/todos/:id', (req, res) => {
   con.query('UPDATE todos SET destroyed = ? Where ID = ?', [1, req.params.id], (err, result) => {
-    errorHandling(err);
-    res.send(JSON.stringify(result));
+    subFunctions.errorHandling(err);
+    res.send(result);
   });
 });
 
---
+++ week-09/day-3/todos/subFunctions.js
@@ -0,0 +1,24 @@
+function errorHandling(err) {
+  if (err) {
+    console.log(err.toString());
+    return;
+  }
+}
+
+function defineBody(body) {
+  return JSON.parse(JSON.stringify(body))
+}
+
+function checkOrUncheck(body) {
+  return defineBody(body).completed ? 1 : 0;
+}
+
+function setNewTodo(task) {
+  return { completed : 0 , text:  task , destroyed : 0 }
+}
+
+
+module.exports.errorHandling = errorHandling;
+module.exports.defineBody = defineBody;
+module.exports.checkOrUncheck = checkOrUncheck;
+module.exports.setNewTodo = setNewTodo;
